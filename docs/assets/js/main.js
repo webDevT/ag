dac326e84e0c4ca98ca7ad42affe80264effd307
webDevT <karinadesyatnik27@gmail.com>
2024-10-12 16:52:47 +0300

    portal

---
+++ docs/assets/js/main.js
@@ -0,0 +1,82 @@
+$(function(){
+
+    $('.menu-btn').click(function(){
+        $(this).toggleClass('active');
+        $('.header__right').toggleClass('active');
+    })
+
+    $('.destination-item__plus').click(function(){
+        $(this).parent().parent().find('.destination-item__content').slideToggle();
+        // $(this).parent().toggleClass('active');
+    })
+
+
+
+    $('.itinerary-item .destination-item__header').click(function(){
+        $(this).parent().find('.destination-item__content').slideToggle();
+        $(this).toggleClass('active');
+    })
+
+
+
+
+
+});
+
+// ------start sticky header------
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 1){
+    $('.header').addClass("sticky");
+    }
+    else{
+    $('.header').removeClass("sticky");
+    }
+    });
+    
+//-------end sticky header---
+
+//start slider
+$(function(){
+$('.slider').slick({
+    slidesToShow: 1,
+    arrows: false,
+    swipe: false,
+    autoplay: true,
+    autoplaySpeed: 8000,
+    speed: 1000,
+    pauseOnHover: false,
+    fade: true
+});
+
+$('.multu-slider').slick({
+    slidesToShow: 4,
+    nextArrow: $('.multu-next'),
+    prevArrow: $('.multu-prev'),
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+              slidesToShow: 3,
+            }
+          },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1,
+          }
+        }
+    ]
+});
+});
+//end slider
+if($(window).width() < 992) {
+
+        $('.menu > ul > li > a').on('click', function(event) {
+          event.preventDefault(); 
+          $(this).siblings('.sub-menu').toggleClass('active');
+         $('.sub-menu-item').toggleClass('active');
+        });
+  
+      
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,56 +1,56 @@
 let gulp = require('gulp'),
-	sass = require('gulp-sass')(require('sass'));
-	browserSync = require('browser-sync'),
-	uglify = require('gulp-uglify'),
-	concat = require('gulp-concat'),
-	rename = require('gulp-rename'),
-	del = require('del'),
-	autoprefixer = require('gulp-autoprefixer');
+    sass = require('gulp-sass')(require('sass')),
+    browserSync = require('browser-sync'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename'),
+    del = require('del'),
+    autoprefixer = require('gulp-autoprefixer');
 
-
-gulp.task('clean', async function(){
-	del.sync('docs')
-})
-
-gulp.task('sass', function(){
-	return gulp.src('app/sass/**/*.sass')
-		.pipe(sass({outputStyle: 'compressed'}))
-		.pipe(autoprefixer({
-			overrideBrowserslist:  ['last 8 versions']
-		}))
-		.pipe(rename({suffix: '.min'}))
-		.pipe(gulp.dest('app/css'))
-		.pipe(browserSync.reload({stream: true}))
+// Очистка папки docs
+gulp.task('clean', async function () {
+    del.sync('docs');
 });
 
-
-gulp.task('css', function(){
-	return gulp.src([
-		'node_modules/normalize.css/normalize.css',
-		'node_modules/slick-carousel/slick/slick.css'
-		])
-	.pipe(sass({outputStyle: 'compressed'}))
-	.pipe(concat('libs.css'))
-	.pipe(rename({suffix: '.min'}))
-	.pipe(gulp.dest('app/css'))
-	.pipe(browserSync.reload({stream: true}))
-})
-
-gulp.task('html', function(){
-	return gulp.src('app/*.html')
-	.pipe(browserSync.reload({stream: true}))
-
+// Обработка SASS файлов
+gulp.task('sass', function () {
+    return gulp.src('app/assets/sass/**/*.sass')
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 8 versions']
+        }))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('app/assets/css'))
+        .pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('script', function(){
-	return gulp.src('app/js/*.js')
-	.pipe(browserSync.reload({stream: true}))
-
+// Обработка CSS файлов
+gulp.task('css', function () {
+    return gulp.src([
+        'node_modules/normalize.css/normalize.css',
+        'node_modules/slick-carousel/slick/slick.css'
+    ])
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(concat('libs.css'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('app/assets/css'))
+        .pipe(browserSync.reload({ stream: true }));
 });
 
+// Обработка HTML файлов
+gulp.task('html', function () {
+    return gulp.src('app/*.html')
+        .pipe(browserSync.reload({ stream: true }));
+});
 
+// Обработка JS файлов
+gulp.task('script', function () {
+    return gulp.src('app/assets/js/*.js')
+        .pipe(browserSync.reload({ stream: true }));
+});
 
-gulp.task('browser-sync', function() {
+// Настройка browser-sync
+gulp.task('browser-sync', function () {
     browserSync.init({
         server: {
             baseDir: "app/"
@@ -58,30 +58,33 @@ gulp.task('browser-sync', function() {
     });
 });
 
-gulp.task('export', function(){
-	let buildHtml = gulp.src('app/**/*.html')
-		.pipe(gulp.dest('docs'));
+// Экспорт файлов в папку docs
+gulp.task('export', function () {
+    let buildHtml = gulp.src('app/**/*.html')
+        .pipe(gulp.dest('docs'));
 
-	let BuildCss = gulp.src('app/css/**/*.css')
-	.pipe(gulp.dest('docs/css'));
+    let buildCss = gulp.src('app/assets/css/**/*.css')
+        .pipe(gulp.dest('docs/assets/css'));
 
-	let BuildJs = gulp.src('app/js/**/*.js')
-	.pipe(gulp.dest('docs/js'));
+    let buildJs = gulp.src('app/assets/js/**/*.js')
+        .pipe(gulp.dest('docs/assets/js'));
 
-	let BuildFonts = gulp.src('app/fonts/**/*.*')
-	.pipe(gulp.dest('docs/fonts'));
+    let buildFonts = gulp.src('app/assets/fonts/**/*.*')
+        .pipe(gulp.dest('docs/assets/fonts'));
 
-
-	let BuildImg = gulp.src('app/img/**/*.*')
-	.pipe(gulp.dest('docs/img'));
+    let buildImg = gulp.src('app/assets/img/**/*.*')
+        .pipe(gulp.dest('docs/assets/img'));
 });
 
-gulp.task('watch', function(){
-	gulp.watch('app/sass/**/*.sass', gulp.parallel('sass'));
-	gulp.watch('app/*.html', gulp.parallel('html'));
-	gulp.watch('app/js/*.js',gulp.parallel('script'));
+// Наблюдение за изменениями в файлах
+gulp.task('watch', function () {
+    gulp.watch('app/assets/sass/**/*.sass', gulp.parallel('sass'));
+    gulp.watch('app/assets/*.html', gulp.parallel('html'));
+    gulp.watch('app/assets/js/*.js', gulp.parallel('script'));
 });
 
+// Сборка проекта
 gulp.task('build', gulp.series('clean', 'export'));
 
-gulp.task('default', gulp.parallel('css', 'sass', 'browser-sync', 'watch'))
+// Задача по умолчанию
+gulp.task('default', gulp.parallel('css', 'sass', 'browser-sync', 'watch'));
